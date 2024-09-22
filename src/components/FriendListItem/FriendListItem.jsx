d79a8957a7b7a81aab8280a87d0c29422ff56fc9
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-09-22 20:33:54 +0200

    fix-task2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,10 @@
-import "./FriendListItem.module.css";
+import styles from "./FriendListItem.module.css";
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p className={isOnline ? "online" : "offline"}>
+      <p className={isOnline ? styles.online : styles.offline}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
